c033cc37e92ac63c387b76b751b3807fe3e93228
Sriram Schelbert <sriram.schelbert@gmail.com>
2025-05-12 11:31:28 -0400

    Added copy directory function

---
+++ deidentifier.py
@@ -0,0 +1,19 @@
+import os
+import shutil 
+from pathlib import Path
+
+def create_deidentified_folder (source_folder): 
+    if not os.path.isdir(source_folder):
+        print(f"The source folder '{source_folder} does not exist")
+        return
+
+    parent = source_folder.parent.absolute()
+    copy_path = "{parent}/Copy"
+
+    try:
+        shutil.copytree(source_folder, parent)
+        print(f"Directory copied from '{source_folder}' to '{copy_path}'")
+    except FileExistsError:
+        print(f"Destination directory '{copy_path}' already exists.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
