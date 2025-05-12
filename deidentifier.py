c4a435bbfa1374b218c0176e872b5281b5f75a91
Sriram Schelbert <sriram.schelbert@gmail.com>
2025-05-12 12:17:21 -0400

    Fixed parent to retrieve the actual parent directory

--- deidentifier.py
+++ deidentifier.py
@@ -3,18 +3,20 @@ import shutil
 from pathlib import Path
 
 def create_deidentified_folder (source_folder): 
-    if not os.path.isdir(source_folder):
-        print(f"The source folder '{source_folder} does not exist")
+    source_path = Path(source_folder)
+
+    if not os.path.isdir(source_path):
+        print(f"The source folder '{source_path} does not exist")
         return
 
-    parent = "/home/sriram/Documents/Test Folder"
-    copy_path = f"{parent}/Test2"
+    parent = source_path.parent
+    deid_path = f"{parent}/deid_{source_path.name}"
 
     try:
-        shutil.copytree(source_folder, copy_path)
-        print(f"Directory copied from '{source_folder}' to '{copy_path}'")
+        shutil.copytree(source_path, deid_path)
+        print(f"Directory copied from '{source_path}' to '{deid_path}'")
     except FileExistsError:
-        print(f"Destination directory '{copy_path}' already exists.")
+        print(f"Destination directory '{deid_path}' already exists.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
