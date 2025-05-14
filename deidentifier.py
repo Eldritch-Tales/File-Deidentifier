0648ef6abf19757a110a4c4cdb72143f9e44e29d
Sriram Schelbert <sriram.schelbert@gmail.com>
2025-05-14 10:17:00 -0400

    Changed name of file-deleting method

--- deidentifier.py
+++ deidentifier.py
@@ -19,14 +19,14 @@ def create_deidentified_folder (source_folder):
         print(f"Directory copied from '{source_path}' to '{deid_path}'")
 
         # Deletes communication log
-        delete_communication_log(deid_path)
+        delete_files_change_name(deid_path)
     except FileExistsError:
         print(f"Destination directory '{deid_path}' already exists.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 # Deletes all files named 'Communication Log' within input directory
-def delete_communication_log(directory):
+def delete_files_change_name(directory):
     # Iterates through all file and directories under the input path
     for file_path in directory.rglob("*"):
         if file_path.is_file() and file_path.stem == "Communication Log":
